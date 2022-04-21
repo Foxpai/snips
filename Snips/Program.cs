f0788f1b9b7879fa0e3becb7aa7e11d7d25f7a76
Foxpal <cookie_man001@yahoo.com>
2022-04-21 07:30:19 -0700

    Add files via upload
    todo

--- Snips/Program.cs
+++ Snips/Program.cs
@@ -7,6 +7,24 @@ namespace Snips
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            /*TODO
+             * Eliptic Curve Function
+             * Logging to Event Viewer
+             * Sin Functions
+             * Fast Forrie Function
+             * Gaussian Function
+             * Quicksort Function
+             * Exchange Array Sorting
+             * Insertion Sorts
+             * REST Clients, Functions, Requests, Paramaters, and Objects
+             */
+
+            /*
+             * For File Creation, Dir Creation, Reading from files see: SystemIO.cs
+             * For Random Number and Password Generators see: RandomGenerators.cs
+             * For Logging to file and Event Viewer(soon) see: Logging.cs
+             */
         }
     }
 }
